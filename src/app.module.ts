23e6dd9426650bb340f6940e9d4b32f4e388c61b
william-esteban <114128992+william-esteban@users.noreply.github.com>
2024-05-08 13:20:02 -0500

    main module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { ConnectionModule } from './libs/persistence/connection.module';
+import dbConfig from './libs/persistence/db.config';
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+    ConnectionModule,
+    UsersModule,
+
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
 
   const config = new DocumentBuilder()
     .setTitle('Authentication API')
