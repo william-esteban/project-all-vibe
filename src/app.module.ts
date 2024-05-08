65e769eb67acdb4985ce1e815b1177a4e05829a1
william-esteban <114128992+william-esteban@users.noreply.github.com>
2024-05-08 11:55:59 -0500

    add module persistence

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import dbConfig from './libs/persistence/db.config';
+import { ConnectionModule } from './libs/persistence/connection.module';
 import { UserModule } from './modules/user/user.module';
 
 
+
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+    ConnectionModule,
+    UserModule],
   controllers: [],
   providers: [],
 })
