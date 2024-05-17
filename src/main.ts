6e741c57a411d4c595bb5d27f52644f6162a1292
william-esteban <114128992+william-esteban@users.noreply.github.com>
2024-05-17 16:39:00 -0500

    create checkout

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Authentication API')
     .setDescription(
